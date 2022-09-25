730fa2b382511ad51c8211434da0c23f5b4a177c
Simon White <simon.white@investisdigital.com>
2022-09-25 17:51:36 +0100

    Fixing comment spacing.

--- docroot/src/pages/Home.js
+++ docroot/src/pages/Home.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore/lite";
 
-//Firebase imports.
+// Firebase imports.
 import { db, auth } from "../services/firebase";
 
 // Theme UI imports.
